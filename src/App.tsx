742cdc6ef750c529d084119859ce1f8b707c1d55
Alexander Shuvalov <alex-shuv@mail.ru>
2024-03-10 17:41:29 +0300

    add favorite-users

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
+import { Link, Route, Routes } from "react-router-dom";
 import { UsersPage } from "./page/users-page/users-page";
 import { UserPage } from "./page/user-page/user-page";
+import { FavoredUsersPage } from "./page/favored-users-page/favored-users-page";
+import FavoritesIcon from "./assets/favorites-icon.svg";
+import "./App.css";
 
 export const App = () => {
   return (
     <div className="app">
+      <Link to="/favorites" className="app-favorite">
+        <p className="app-title">Избранное</p>
+        <img src={FavoritesIcon} alt="Избранное" />
+      </Link>
       <Routes>
+        <Route path="/favorites" element={<FavoredUsersPage />} />
         <Route path="/" element={<UsersPage />} />
         <Route path="/:id" element={<UserPage />} />
       </Routes>
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,38 +1,68 @@
 import { create } from "zustand";
-import { UserState } from "./store-types";
+import { Posts, User, UserState } from "./store-types";
 import { immer } from "zustand/middleware/immer";
+import { persist } from "zustand/middleware";
 import axios from "axios";
 
 export const useUsersStore = create<UserState>()(
-  immer((set) => ({
-    users: [],
-    posts: [],
-    user: {
-      id: 0,
-      name: "",
-      username: "",
-      email: "",
-      phone: "",
-      website: "",
-    },
-    page: 1,
-    getUsersList: async () => {
-      const { data } = await axios.get(
-        "http://jsonplaceholder.typicode.com/users?_start=0&_limit=10"
-      );
-      set({ users: data });
-    },
-    getUser: async (id: number) => {
-      const { data } = await axios.get(
-        `https://jsonplaceholder.typicode.com/users/${id}`
-      );
-      set({ user: data });
-    },
-    getPosts: async (id: number) => {
-      const { data } = await axios.get(
-        `https://jsonplaceholder.typicode.com/posts?userId=${id}`
-      );
-      set({ posts: data });
-    },
-  }))
+  persist(
+    immer((set) => ({
+      users: [],
+      posts: [],
+      user: {
+        id: 0,
+        name: "",
+        username: "",
+        email: "",
+        phone: "",
+        website: "",
+        isFavorite: false,
+      },
+      favoriteUsers: [],
+
+      getUsersList: async (skip: number) => {
+        const { data } = await axios.get(
+          `http://jsonplaceholder.typicode.com/users?_start=${skip}&_limit=10`
+        );
+        set({ users: data });
+      },
+      getUser: async (id: number, state?: User) => {
+        if (id === state?.id) {
+          return;
+        }
+        const { data } = await axios.get<User>(
+          `https://jsonplaceholder.typicode.com/users/${id}`
+        );
+
+        set((state) => {
+          const modifiedData = {
+            ...data,
+            isFavorite: state.favoriteUsers.some((user) => user.id === data.id),
+          };
+          state.user = { ...modifiedData };
+        });
+      },
+      getPosts: async (id: number) => {
+        const { data } = await axios.get<Posts[]>(
+          `https://jsonplaceholder.typicode.com/posts?userId=${id}`
+        );
+        set({ posts: data });
+      },
+      toggleFavoriteUser: (userId: number) => {
+        set((state) => {
+          if (state.user && state.user.id === userId) {
+            state.user.isFavorite = !state.user.isFavorite;
+            if (state.user.isFavorite) {
+              state.favoriteUsers.push(state.user);
+            } else {
+              state.favoriteUsers = state.favoriteUsers.filter(
+                (user) => user.id !== state.user.id
+              );
+            }
+          }
+        });
+      },
+    })),
+    { name: "users-store", version: 1 }
+  )
 );
